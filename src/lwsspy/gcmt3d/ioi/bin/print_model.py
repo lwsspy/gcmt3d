4c1c74c1f922a98ca14e892e946a7984eb2bc688
Lucas Sawade <lsawade@princeton.edu>
2022-03-06 22:54:26 -0500

    fogrgot to add print_model.py

---
+++ src/lwsspy/gcmt3d/ioi/bin/print_model.py
@@ -0,0 +1,58 @@
+"""
+:Author:
+    Lucas Sawade (lsawade-at-princeton.edu)
+
+:Last Modified:
+    2022.03.06 16.00
+
+"""
+
+from sys import argv
+from os import path, listdir
+from numpy import load, array2string
+
+def bin():
+    """
+
+    Usage: 
+
+        gcmt3d-print-model eventdir [anything]
+
+    This script calls a python function that prints the costs. If any 
+    command line arguments are included the function includes the linesearches.
+
+    where path to 'event_status' and 'label' are found in the 'input.yml'.
+
+    After initialization.
+    
+
+    """
+
+
+    # Get args or print usage statement
+    if (len(argv) < 2) or (len(argv) > 3) or (argv[1] == '-h') or (argv[1] == '--help'):
+        print(bin.__doc__)
+        exit()
+    else:
+        eventdir = path.abspath(argv[1])
+        lsflag = True if len(argv) == 3 else False
+
+    # Print costs
+    modldir = path.join(eventdir, 'modl')
+
+    for file in sorted(listdir(modldir)):
+
+        # Combine the filename
+        filename = path.join(modldir, file)
+
+        # Get iteration and step from filename
+        _, it, ls = file.split("_")
+        it = int(it[2:])
+        ls = int(ls[2:-4])
+
+        # Check filename for iteration and linesearch number
+        if lsflag:
+            print(f"Iter/Step: {it:0>5}/{ls:0>5} -> Model: {load(filename):f}")
+        else:
+            if ls == 0:
+                print(f"Iter: {it:0>5} -> Model: {array2string(load(filename), max_line_width=1e10):f}")
